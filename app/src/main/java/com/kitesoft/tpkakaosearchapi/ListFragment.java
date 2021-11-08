14ab5be743ffe70aded9f732fce3785f1c3fde6e
oops <96kite@gmail.com>
2021-11-09 00:48:35 +0900

    a

--- app/src/main/java/com/kitesoft/tpkakaosearchapi/ListFragment.java
+++ app/src/main/java/com/kitesoft/tpkakaosearchapi/ListFragment.java
@@ -8,8 +8,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ListFragment extends Fragment {
+
+    RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
